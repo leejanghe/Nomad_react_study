967332030e62eecb5c966808104490ddc94d40fb
leejanghe <babybye777@gmail.com>
2022-12-13 23:42:39 +0900

    theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   const [value, setValue] = useState("");
@@ -23,6 +31,9 @@ function App() {
         />
         <button>Log in</button>
       </form>
+      <Container>
+        <H1>hello</H1>
+      </Container>
     </div>
   );
 }
